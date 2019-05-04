95cf53e3732909eca1069ec3a0b692e51b19f5d7
KARAN SHRESTHA <karan_shrestah1@outlook.com>
2019-05-04 16:15:02 +0545

    Initial commit

---
+++ app/src/main/java/com/a3rd_assign/MainActivity.java
@@ -0,0 +1,47 @@
+package com.a3rd_assign;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnAddItem, btnDashboard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAddItem = findViewById(R.id.btnAddItem);
+        btnDashboard = findViewById(R.id.btnDashboard);
+
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, additem.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        btnDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, dashboard.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+}
